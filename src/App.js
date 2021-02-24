7313fdba4f7d0e958be6acfc4ca6643d0eacde44
oddwes <oddwes777@gmail.com>
2021-02-24 15:01:45 -0500

    implements header and blank body

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from 'react';
+import { Card, Container, Navbar } from "react-bootstrap";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Card text="white">
+        <Card.Header style={{backgroundColor: "#30353F"}}>
+          <h1>Julian Lai</h1>
+          <h3>An enthusiastic developer looking for his next project to obsess over</h3>
+          <div>
+            <b>Phone</b> (514) 962-4384
+          </div>
+          <div>
+            <b>E-mail</b> julianlai.777@gmail.com
+          </div>
+          <div>
+            <b>Linkedin</b> linkedin.com/in/julian-lai-06223795
+          </div>
+        </Card.Header>
+        <Card.Body>
+
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
 

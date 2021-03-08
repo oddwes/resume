332c28d4f4611fb6e434b95a5eef8925d2718ae4
oddwes <oddwes777@gmail.com>
2021-03-08 13:04:40 -0500

    adds personal projects section

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Line from "./Line";
 
 function App() {
   return (
-    <Container style={{padding: '20px'}}>
+    <Container style={{ padding: '20px' }}>
       <Card>
-        <Card.Header style={{color: 'white', backgroundColor: "#30353F"}}>
+        <Card.Header style={{ color: 'white', backgroundColor: "#30353F" }}>
           <h1>Julian Lai</h1>
           <h3>An enthusiastic developer looking for his next project to obsess over</h3>
           <div>
@@ -32,7 +32,7 @@ function App() {
               </Col>
               <Col>
                 <p>
-                  <b>System Integrations Developer</b><br/>
+                  <b>System Integrations Developer</b><br />
                   <i>Diff Agency, Montreal, QC</i>
                 </p>
                 <p className="pl-4">
@@ -51,7 +51,7 @@ function App() {
               </Col>
               <Col>
                 <p>
-                  <b>Java developer</b><br/>
+                  <b>Java developer</b><br />
                   <i>123Certification, Montreal, QC</i>
                 </p>
                 <p className="pl-4">
@@ -67,6 +67,20 @@ function App() {
             </Row>
           </Container>
           <Container>
+            <h3>Personal Projects</h3>
+            <Line />
+            <Container className="pl-4">
+              <p>
+                <b>Resume Webpage</b><br />
+              </p>
+              <p className="pb-4 pl-2">
+                <List bulleted>
+                  <List.Item>Technologies: React, Docker, Kubernetes, Gitlab CI/CD</List.Item>
+                </List>
+              </p>
+            </Container>
+          </Container>
+          <Container>
             <h3>Education</h3>
             <Line />
             <Row>
@@ -74,7 +88,7 @@ function App() {
                 <b>2011-09 ~ 2016-04</b>
               </Col>
               <Col>
-                <b>Bachelar of Engineering (Computer Engineering)</b><br/>
+                <b>Bachelar of Engineering (Computer Engineering)</b><br />
                 <i>Concordia University</i>
               </Col>
             </Row>
@@ -83,7 +97,7 @@ function App() {
                 <b>2011-09 ~ 2016-04</b>
               </Col>
               <Col>
-                <b>Bachelor of Science: Pure and Applied Sciences</b><br/>
+                <b>Bachelor of Science: Pure and Applied Sciences</b><br />
                 <i>Marianopolis College</i>
               </Col>
             </Row>
@@ -96,7 +110,7 @@ function App() {
                 <b>2014-09 ~ 2015-03</b>
               </Col>
               <Col>
-                <b>Orchestr8 - Capstone Project</b><br/>
+                <b>Orchestr8 - Capstone Project</b><br />
                 Musical interpreter, music sheet generator and robotic piano player
               </Col>
             </Row>
@@ -105,7 +119,7 @@ function App() {
                 <b>2013-09 ~ 2013-12</b>
               </Col>
               <Col>
-                <b>"Sumo" Robot</b><br/>
+                <b>"Sumo" Robot</b><br />
                 Atmega8 microcontroller, edge detection and opponent tracking
               </Col>
             </Row>
@@ -114,8 +128,8 @@ function App() {
             <h3>Technologies</h3>
             <Line />
             <div className="pl-4">
-              Ruby on Rails, React, NodeJS<br/>
-              Docker, Kuberneters, AWS, Ansible, Git<br/>
+              Ruby on Rails, React, NodeJS<br />
+              Docker, Kubernetes, AWS, Ansible, Git<br />
               Mac, Linux, Windows
             </div>
           </Container>

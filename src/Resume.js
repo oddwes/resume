8cb1baad41f0ec6efbfef0cf163b01b9f15efbc3
oddwes <oddwes777@gmail.com>
2022-02-03 10:21:50 -0800

    update

--- src/Resume.js
+++ src/Resume.js
@@ -4,177 +4,142 @@ import { Card, Container, Row, Col } from "react-bootstrap";
 import { List } from 'semantic-ui-react';
 import Line from "./Line";
 
+import './Resume.css'
+
 class Resume extends React.Component {
   render () {
     return (
-      <Container style={{ padding: '20px' }}>
-        <Card>
-          <Card.Header style={{ color: 'white', backgroundColor: "#30353F" }}>
-            <h1>Julian Lai</h1>
-            <h3>An enthusiastic developer looking for his next project to obsess over</h3>
-            <div>
-              <b>Phone</b> (514) 962-4384
-            </div>
-            <div>
-              <b>E-mail</b> julianlai.777@gmail.com
-            </div>
-            <div>
-              <b>Linkedin</b> linkedin.com/in/julian-lai-06223795
-            </div>
-          </Card.Header>
-          <Card.Body text="black">
-            <Container>
-              <h3>Work</h3>
-              <Line />
+			<Container>
+				<Card>
+					<Card.Header style={{ color: 'white', backgroundColor: "#30353F" }}>
+						<h1>Julian Lai</h1>
+						<h3>An enthusiastic developer looking for his next project to obsess over</h3>
+						<div>
+							<b>Phone</b> (514) 962-4384
+						</div>
+						<div>
+							<b>E-mail</b> julianlai.777@gmail.com
+						</div>
+						<div>
+							<b>Linkedin</b> linkedin.com/in/julian-lai-06223795
+						</div>
+					</Card.Header>
+					<Card.Body text="black">
+						<Container>
+							<h3>Work</h3>
+							<Line />
 							<Row className="pb-4">
-                <Col sm={2} className="centered">
-                  <b>2022-01 ~ Present</b>
-                </Col>
-                <Col>
-                  <p>
-                    <b>Senior Developer</b><br />
-                    <i>Jane.app, Vancouver, BC</i>
-                  </p>
-                </Col>
-              </Row>
-              <Row className="pb-4">
-                <Col sm={2} className="centered">
-                  <b>2021-04 ~ 2022-01</b>
-                </Col>
-                <Col>
-                  <p>
-                    <b>Ruby Developer</b><br />
-                    <i>Jane.app, Vancouver, BC</i>
-                  </p>
-                  <p className="pl-4">
-                      <List bulleted>
-                        <List.Item>Project lead for Waitlist Notifications</List.Item>
-                      </List>
-                    </p>
-                </Col>
-              </Row>
-              <Row className="pb-4">
-                <Col sm={2} className="centered">
-                  <b>2017-11 ~ 2021-04</b>
-                </Col>
-                <Col>
-                  <p>
-                    <b>System Integrations Developer</b><br />
-                    <i>Diff Agency, Montreal, QC</i>
-                  </p>
-                  <p className="pl-4">
-                    <List bulleted>
-                      <List.Item>Design and development of custom middleware integrating Shopify with ERPs, WMSs, carrier services</List.Item>
-                      <List.Item>Design and development of user-friendly admin dashboards which include job scheduling, product management, order processing, reporting</List.Item>
-                      <List.Item>Developed ramp-up project to familiarize new developers with common integration business requirements and tech stack</List.Item>
-                      <List.Item>Technologies: Ruby on Rails, Postgresql, AWS, Ansible, Capistrano, Git</List.Item>
-                    </List>
-                  </p>
-                </Col>
-              </Row>
-              <Row className="pb-4">
-                <Col sm={2} className="centered">
-                  <b>2015-09 ~ 2017-10</b>
-                </Col>
-                <Col>
-                  <p>
-                    <b>Java Developer</b><br />
-                    <i>123Certification, Montreal, QC</i>
-                  </p>
-                  <p className="pl-4">
-                    <List bulleted>
-                      <List.Item>Welding simulator (Arc+)</List.Item>
-                      <List.Item>3D visual tracking system integration with calibration module</List.Item>
-                      <List.Item>Visual tracking system integration. Tracking calibration module</List.Item>
-                      <List.Item>Setup of SVN and Jenkins servers</List.Item>
-                      <List.Item>Technologies: Java, Jenkins, SVN</List.Item>
-                    </List>
-                  </p>
-                </Col>
-              </Row>
-            </Container>
-            <Container>
-              <h3>Personal Projects</h3>
-              <Line />
-              <Container className="pl-4">
-                <p>
-                  <b>StravaBoards</b><br />
-                </p>
-                <p className="pb-4 pl-2">
-                  <List bulleted>
-                    <List.Item>Technologies: React, Ruby on Rails, Docker, Kubernetes, Gitlab CI/CD</List.Item>
-                    <List.Item><a href="https://stravaboards.oddwes.ca" target="_blank" rel="noreferrer">stravaboards.oddwes.ca</a></List.Item>
-                  </List>
-                </p>
-              </Container>
-              <Container className="pl-4">
-                <p>
-                  <b>Resume Webpage</b><br />
-                </p>
-                <p className="pb-4 pl-2">
-                  <List bulleted>
-                    <List.Item>Technologies: React, Docker, Kubernetes, Gitlab CI/CD</List.Item>
-                    <List.Item><a href="https://resume.oddwes.ca" target="_blank" rel="noreferrer">resume.oddwes.ca</a></List.Item>
-                  </List>
-                </p>
-              </Container>
-            </Container>
-            <Container>
-              <h3>Education</h3>
-              <Line />
-              <Row>
-                <Col sm={2} className="centered">
-                  <b>2011-09 ~ 2016-04</b>
-                </Col>
-                <Col>
-                  <b>Bachelor of Engineering (Computer Engineering)</b><br />
-                  <i>Concordia University</i>
-                </Col>
-              </Row>
-              <Row className="py-4">
-                <Col sm={2} className="centered">
-                  <b>2009-09 ~ 2011-04</b>
-                </Col>
-                <Col>
-                  <b>Bachelor of Science (Pure and Applied Sciences)</b><br />
-                  <i>Marianopolis College</i>
-                </Col>
-              </Row>
-            </Container>
-            <Container>
-              <h3>School Projects</h3>
-              <Line />
-              <Row>
-                <Col sm={2} className="centered">
-                  <b>2014-09 ~ 2015-03</b>
-                </Col>
-                <Col>
-                  <b>Orchestr8 - Capstone Project</b><br />
-                  Musical interpreter, music sheet generator and robotic piano player
-                </Col>
-              </Row>
-              <Row className="py-4">
-                <Col sm={2} className="centered">
-                  <b>2013-09 ~ 2013-12</b>
-                </Col>
-                <Col>
-                  <b>"Sumo" Robot</b><br />
-                  Atmega8 microcontroller, edge detection and opponent tracking
-                </Col>
-              </Row>
-            </Container>
-            <Container>
-              <h3>Technologies</h3>
-              <Line />
-              <div className="pl-4">
-                Ruby on Rails, React, NodeJS<br />
-                Docker, Kubernetes, AWS, Ansible, Git<br />
-                Mac, Linux, Windows
-              </div>
-            </Container>
-          </Card.Body>
-        </Card>
-      </Container>
+								<Col sm={2} className="centered">
+									<b>2021-04 ~ Present</b>
+								</Col>
+								<Col>
+									<p>
+										<b>Senior Ruby Developer</b><br />
+										<i>Jane.app, Vancouver, BC</i>
+									</p>
+									<p className="pl-4">
+											<List bulleted>
+												<List.Item>Project lead for Waitlist Notifications</List.Item>
+											</List>
+										</p>
+								</Col>
+							</Row>
+							<Row className="pb-4">
+								<Col sm={2} className="centered">
+									<b>2017-11 ~ 2021-04</b>
+								</Col>
+								<Col>
+									<p>
+										<b>System Integrations Developer</b><br />
+										<i>Diff Agency, Montreal, QC</i>
+									</p>
+									<p className="pl-4">
+										<List bulleted>
+											<List.Item>Design and development of custom middleware integrating Shopify with ERPs, WMSs, carrier services</List.Item>
+											<List.Item>Design and development of user-friendly admin dashboards which include job scheduling, product management, order processing, reporting</List.Item>
+											<List.Item>Developed ramp-up project to familiarize new developers with common integration business requirements and tech stack</List.Item>
+											<List.Item>Technologies: Ruby on Rails, Postgresql, AWS, Ansible, Capistrano, Git</List.Item>
+										</List>
+									</p>
+								</Col>
+							</Row>
+							<Row className="pb-4">
+								<Col sm={2} className="centered">
+									<b>2015-09 ~ 2017-10</b>
+								</Col>
+								<Col>
+									<p>
+										<b>Java Developer</b><br />
+										<i>123Certification, Montreal, QC</i>
+									</p>
+									<p className="pl-4">
+										<List bulleted>
+											<List.Item>Welding simulator (Arc+)</List.Item>
+											<List.Item>3D visual tracking system integration with calibration module</List.Item>
+											<List.Item>Visual tracking system integration. Tracking calibration module</List.Item>
+											<List.Item>Setup of SVN and Jenkins servers</List.Item>
+											<List.Item>Technologies: Java, Jenkins, SVN</List.Item>
+										</List>
+									</p>
+								</Col>
+							</Row>
+						</Container>
+						<Container>
+							<h3>Education</h3>
+							<Line />
+							<Row>
+								<Col sm={2} className="centered">
+									<b>2011-09 ~ 2016-04</b>
+								</Col>
+								<Col>
+									<b>Bachelor of Engineering (Computer Engineering)</b><br />
+									<i>Concordia University</i>
+								</Col>
+							</Row>
+							<Row className="py-4">
+								<Col sm={2} className="centered">
+									<b>2009-09 ~ 2011-04</b>
+								</Col>
+								<Col>
+									<b>Bachelor of Science (Pure and Applied Sciences)</b><br />
+									<i>Marianopolis College</i>
+								</Col>
+							</Row>
+						</Container>
+						<Container>
+							<h3>School Projects</h3>
+							<Line />
+							<Row>
+								<Col sm={2} className="centered">
+									<b>2014-09 ~ 2015-03</b>
+								</Col>
+								<Col>
+									<b>Orchestr8 - Capstone Project</b><br />
+									Musical interpreter, music sheet generator and robotic piano player
+								</Col>
+							</Row>
+							<Row className="py-4">
+								<Col sm={2} className="centered">
+									<b>2013-09 ~ 2013-12</b>
+								</Col>
+								<Col>
+									<b>"Sumo" Robot</b><br />
+									Atmega8 microcontroller, edge detection and opponent tracking
+								</Col>
+							</Row>
+						</Container>
+						<Container>
+							<h3>Technologies</h3>
+							<Line />
+							<div className="pl-4">
+								Ruby on Rails, React, NodeJS<br />
+								Docker, Kubernetes, AWS, Ansible, Git<br />
+								Mac, Linux, Windows
+							</div>
+						</Container>
+					</Card.Body>
+				</Card>
+			</Container>
     )
   }
 }

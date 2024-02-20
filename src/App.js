011fa6e7e97d5089c98948e95843a54155bc18c6
oddwes <oddwes777@gmail.com>
2024-02-19 19:00:44 -0800

    removes print button

--- src/App.js
+++ src/App.js
@@ -4,16 +4,9 @@ import { useReactToPrint } from 'react-to-print';
 import Resume from "./Resume";
 
 function App() {
-  const componentRef = useRef();
-  const handlePrint = useReactToPrint({
-    content: () => componentRef.current,
-  })
-
   return (
     <>
-      <button className="rounded-button" onClick={handlePrint}>Print to PDF</button>
-      <br />
-      <Resume ref={componentRef}/>
+      <Resume/>
     </>
   )
 }
